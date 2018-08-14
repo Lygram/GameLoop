cc4ce1f58b2a3266dc2788198ac0445c1bacc697
Lygram <42025546+Lygram@users.noreply.github.com>
2018-08-14 18:56:36 +0300

    Add files via upload

---
+++ GameLoop/FastLoop.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace GameLoop
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Message
+    {
+        public IntPtr hWnd;
+        public Int32 msg;
+        public IntPtr wParam;
+        public IntPtr lParam;
+        public uint time;
+        public System.Drawing.Point p;
+    }
+    public class FastLoop
+    {
+        PerciseTimer _timer = new PerciseTimer();
+        public delegate void LoopCallback(double elapsedTime);
+        [System.Security.SuppressUnmanagedCodeSecurity]
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern bool PeekMessage(
+            out Message msg,
+            IntPtr hWnd,
+            uint messageFilterMin,
+            uint messageFilterMax,
+            uint flags);
+
+        LoopCallback _callback;
+        public FastLoop(LoopCallback callback)
+        {
+            _callback = callback;
+            Application.Idle += new EventHandler(OnApplicationEnterIdle);
+        }
+        void OnApplicationEnterIdle(object Sender, EventArgs e)
+        {
+            while (IsAppStillIdle())
+            {
+                _callback(_timer.GetElapsedTime());
+            }
+        }
+        private bool IsAppStillIdle()
+        {
+            Message msg;
+            return !PeekMessage(out msg, IntPtr.Zero, 0, 0, 0);
+        }
+        
+    }
+}
---
+++ GameLoop/PerciseTimer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace GameLoop
+{
+    class PerciseTimer
+    {
+        [System.Security.SuppressUnmanagedCodeSecurity]
+        [DllImport("kernel132")]
+        private static extern bool QueryPerformanceFrequency(ref long PerformanceFrequency);
+        [System.Security.SuppressUnmanagedCodeSecurity]
+        [DllImport("kernel132")]
+        private static extern bool QueryPerformanceCounter(ref long PerformanceCount);
+        long _ticksPerSecond = 0;
+        long _previousElapsedTime = 0;
+        public PerciseTimer()
+        {
+            QueryPerformanceFrequency(ref _ticksPerSecond);
+            GetElapsedTime();
+        }
+        public double GetElapsedTime()
+        {
+            long time = 0;
+            QueryPerformanceCounter(ref time);
+            double elapsedTime = (double)(time - _previousElapsedTime) / (double)_ticksPerSecond;
+            _previousElapsedTime = time;
+            return elapsedTime;
+        }
+    }
+}
